1bf0871f5ab2b6df082db43b3b6e69e79b379055
mazin <60102524@udst.edu.qa>
2024-01-31 12:34:37 +0300

    initial commit in local-2

--- cars.py
+++ cars.py
@@ -1,5 +1,9 @@
 import random
 
+def randModelYear():
+    return random.randrange(1990, 2025)
+
 carList = ["toyota", "nissan", "honda", "ford", "mazda", "suzuki"]
 
+print(random.choice(carList))
 print(randModelYear())
